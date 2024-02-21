970f3561eae6108e222ba642a9128e705ff86d93
Adrianrobcru <adrian.rob.cru.1@educa.jcyl.es>
2024-02-21 12:44:52 +0100

    Factorial de un numero con recursividad

--- Recursividad/src/es/juana/recursividad/Recursividad.java
+++ Recursividad/src/es/juana/recursividad/Recursividad.java
@@ -1,5 +1,19 @@
 package es.juana.recursividad;
 
 public class Recursividad {
-
+	
+	public static void main(String[] args) {
+		
+		int factorial=factorial(5);
+		System.out.println(factorial);
+		
+	}
+	
+	public static int factorial(int num) {
+		
+		if (num==1) {return 1;}
+		return num*factorial(num-1);
+		
+	}
+	
 }
