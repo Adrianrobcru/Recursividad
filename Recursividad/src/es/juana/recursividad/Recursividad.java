433d076fd935bb579b487419b6f98d0e6790cbfa
Adrianrobcru <adrian.rob.cru.1@educa.jcyl.es>
2024-02-22 12:47:04 +0100

    Creacion del metodo invertido,potencia,invertido,fibonacci,numeros digitos,imprimirdesde e imprimirhasta

--- Recursividad/src/es/juana/recursividad/Recursividad.java
+++ Recursividad/src/es/juana/recursividad/Recursividad.java
@@ -4,8 +4,7 @@ public class Recursividad {
 	
 	public static void main(String[] args) {
 		
-		int factorial=factorial(5);
-		System.out.println(factorial);
+		invertido(12345);
 		
 	}
 	
@@ -15,5 +14,65 @@ public class Recursividad {
 		return num*factorial(num-1);
 		
 	}
+	public static void Imprimirhastan(int n) {
+		
+		if(n>1) {
+		Imprimirhastan(n-1);
+		
+		}
+		System.out.println(n);
+		
+	}
+	public static void imprimirDesde(int n) {
+		
+		System.out.println(n);
+		
+	if(n>1) {
+		
+		imprimirDesde(n-1);
+		
+	}
+		
+		
+		
+	}
+	
+	public static int numeroDigitos(int n) {
+		int Digitos=0;
+		
+		if (n>0) {
+			Digitos =1+numeroDigitos(n/10);
+		}
+		
+		return Digitos;
+		
+	}
+	
+	public static int fibonacci(int n) {
+		if (n==0) {
+			return 0;
+		}
+		if (n==1) {
+			return 1;
+		}
+		
+		return fibonacci(n-1)+fibonacci(n-2);
+		
+	}
+	
+	public static int potencia(int base,int exponente) {
+		if(exponente==0) {return 1;}
+		
+		return base*potencia(base, exponente-1);
+		
+	}
+	public static void invertido(double n) {
+		System.out.print((int)n%10);
+		if((int) n>=10) {
+			invertido(n/10);
+		}
+		
+	}
 	
 }
+
